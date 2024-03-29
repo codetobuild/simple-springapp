42551f37d7d485ac5ea720ff8eca58f607185d25
codetobuild <goodnakha@gmail.com>
2024-03-29 13:51:02 +0530

    updated request path

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -4,14 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/")
 public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome to simple spring app";
 	}
 
 	public static void main(String[] args) {
